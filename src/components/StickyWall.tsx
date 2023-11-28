91a411167bf505d6b645a265f1c1a7377c27b7e6
Julian Quenet <julianquenet8@gmail.com>
2023-11-28 14:15:31 +0200

    Worked on the functionality and styling

--- src/components/StickyWall.tsx
+++ src/components/StickyWall.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect} from "react";
 import AddIcon from '@mui/icons-material/Add';
 
 //All this is still proof of concept, but just getting a basic idea going 
@@ -7,46 +7,45 @@ interface noteProps{
     details : string,
     index : number
 }
-
+const notesArr: any[] = []
 export const StickyWall = ()=>{
-  const notesArr = []
-  
+  const [notesTsx, setNotesTsx] = useState<any>([])
  
+  function StickyNote(props:noteProps){
+  const {title, details, index} = props
+  const [newTitle, setNewTitle] = useState<any>("")
+  const [newDetails, setNewDetails] = useState<any>("")
+  const [confirmTitle, setConfirmTitle] = useState<Boolean>(false) // Will use this as a button later to confirm changes
+  const [confirmDetails, setConfirmDetails] = useState<Boolean>(false) // Will use this as a button later to confirm changes
 
-   function StickyNote(props:noteProps){
-    const {title, details, index} = props
-    const [newTitle, setNewTitle] = useState<any>("")
-    const [newDetails, setNewDetails] = useState<any>("")
-    const [confirmTitle, setConfirmTitle] = useState<Boolean>(false) // Will use this as a button later to confirm changes
-    const [confirmDetails, setConfirmDetails] = useState<Boolean>(false) // Will use this as a button later to confirm changes
+  const note ={
+      title,
+      details,
+      index,
+  }
 
-    const note ={
-        title,
-        details,
-        index,
-    }
-
-    function handleChangeTitle(e:any){ //Will change the type later
-       setNewTitle(e.target.value)
-    }
-    function handleChangeDetails(e:any){ //Will change the type later
-       setNewDetails(e.target.value)
-    }
+  function handleChangeTitle(e:any){ //Will change the type later
+     setNewTitle(e.target.value)
+  }
+  function handleChangeDetails(e:any){ //Will change the type later
+     setNewDetails(e.target.value)
+  }
 
 
-     return (
-       <div style={{width:"250px", height:"280px"}}>
-        <div> 
-            <input name="Title" value={newTitle} onChange={handleChangeTitle}>
-           </input>
-           </div>
-           <div> 
-            <input name="Details" value={newDetails} onChange={handleChangeDetails}>
-           </input>
-           </div>
-       </div>
-     )
-   }
+   return (
+     <div className="note" style={{width:"250px", height:"250px", backgroundColor:"red"}}>
+      <div className="note-title"> 
+          <textarea cols={24} maxLength={55} name="Title" value={newTitle} onChange={handleChangeTitle} placeholder="Title">
+         </textarea>
+         </div>
+         <div className="note-details"> 
+          <textarea rows={10} cols={28} maxLength={33*10} name="Details" value={newDetails} onChange={handleChangeDetails} placeholder="Details">
+         </textarea>
+         </div>
+     </div>
+   )
+ }
+  
 
    function handleClick(){
     const newNote:noteProps = {
@@ -54,8 +53,8 @@ export const StickyWall = ()=>{
        details : "",
        index : notesArr.length
     }
-
-    notesArr.push(newNote)
+    
+    setNotesTsx([...notesTsx, newNote]);
    }
 
     function Card(){
@@ -68,13 +67,19 @@ export const StickyWall = ()=>{
         )
     }
 
-  //Couldn't really see how the component looked as npm run dev doesn't seem to be working but just creating 
-  // a scaffold for now
     return(
         <>
         <div style={{backgroundColor:"white", width:"75vw", height:"100vh", marginLeft:"auto"}}>
-            <h2>Sticky Wall</h2>
-          <div style={{margin:"10px", border:"solid 1px gray", display:"flex", flexWrap:"wrap"}}>
+            <h2 style={{marginLeft:"26px"}}>Sticky Wall</h2>
+          <div style={{ width:"92.5%", height:"82%", margin:"0 auto", border:"solid 1px gray", display:"flex", flexWrap:"wrap", 
+          gap: "25px", overflow:"auto", overflowX:"hidden",
+          padding:"10px"}}>
+           {notesTsx.map((note:noteProps)=>{
+      return(
+        <StickyNote key={note.index} {...note}/>
+        
+      )
+    })}
            <Card />
           </div>
         </div>
@@ -82,3 +87,4 @@ export const StickyWall = ()=>{
     )
 
 }
+
